49362a1dac0007d3f9f786c4cf1d4232996ed62e
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-12-17 11:56:06 -0600

    Adding instructions for wsl in drive e:

--- preinstall.ps1
+++ preinstall.ps1
@@ -1,8 +1,15 @@
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
 Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
 
+# Para instalar desde store
 Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu.appx -UseBasicParsing
 Add-AppxPackage .\Ubuntu.appx
+
+# Para instalar en disco e:
+#Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile e:\Ubuntu.zip -UseBasicParsing
+#Add-AppxPackage e:\Ubuntu.appx
+#Expand-Archive e:\Ubuntu.zip
+# Ahora ejecute "ubuntu.exe" dentro de la carpeta extraída
 
 $params = @{
   Name = "alfresco"
