edad769b07d70c851428c6498322a5cd03ddc020
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-12-18 12:29:06 -0600

    Fixing TLS configuration

--- alfinstall.sh
+++ alfinstall.sh
@@ -400,9 +400,11 @@ EOF
   sudo mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
   sudo mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.sample
   sudo curl -c 5  -# -o /etc/nginx/nginx.conf $BASE_DOWNLOAD/nginx/nginx.conf
-  sudo curl -c 5  -# -o /etc/nginx/conf.d/alfresco.conf $BASE_DOWNLOAD/nginx/alfresco.conf
-  sudo curl -c 5  -# -o /etc/nginx/conf.d/alfresco.conf.ssl $BASE_DOWNLOAD/nginx/alfresco.conf.ssl 
-  sudo curl -c 5  -# -o /etc/nginx/conf.d/basic-settings.conf $BASE_DOWNLOAD/nginx/basic-settings.conf
+  #sudo curl -c 5  -# -o /etc/nginx/conf.d/alfresco.conf $BASE_DOWNLOAD/nginx/alfresco.conf
+  #sudo curl -c 5  -# -o /etc/nginx/conf.d/alfresco.conf.ssl $BASE_DOWNLOAD/nginx/alfresco.conf.ssl 
+  #sudo curl -c 5  -# -o /etc/nginx/conf.d/basic-settings.conf $BASE_DOWNLOAD/nginx/basic-settings.conf
+  sudo curl -c 5 /etc/nginx/sites-available/default $BASE_DOWNLOAD/nginx/default
+  ( cd /etc/nginx/sites-enabled ; sudo ln -s ../sites-available/default . )
   sudo mkdir -p /var/cache/nginx/alfresco
   # Make the ssl dir as this is what is used in sample config
   sudo mkdir -p /etc/nginx/ssl
