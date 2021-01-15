c9703524d2b2d6a6e179fc05efe04a8518412cdb
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-01-15 10:22:16 -0600

    Alflytics in actions

--- alflytics.sh
+++ alflytics.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
+sudo apt-get install -qy openjdk-8-jre-headless openjdk-8-jdk-headless unzip
 wget https://phoenixnap.dl.sourceforge.net/project/pentaho/Business%20Intelligence%20Server/7.1/pentaho-server-ce-7.1.0.0-12.zip
 
-sudo apt-get install -qy openjdk-8-jre-headless openjdk-8-jdk-headless unzip
-
-unzip -d /opt $OLDPWD/pentaho-server-ce-7.1.0.0-12.zip
+unzip -d /opt pentaho-server-ce-7.1.0.0-12.zip
 
 sed -i 's/8080/7080/g' /opt/pentaho-server/tomcat/conf/server.xml
 
