8a6a592dd14ddb2e198b4f8ed64de479622d2cb5
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-02-25 12:35:25 -0600

    Fixing backup script

--- scripts/start-backup.sh
+++ scripts/start-backup.sh
@@ -5,13 +5,16 @@ then
     sudo mkdir -p /mnt/e/alfresco_pgsql
 fi
 
-
 # Crea archivo de cron
-echo EOF | sudo tee /etc/cron.d/pg_dump
-50 9,12,15,20 * * * postgres /usr/bin/pg_dump -Fc -U postgres alfresco -f /mnt/e/alfresco_pgsql/respaldo-bd-$(date +%F-%s).sql.gz 2>&1 > /dev/null
+cat << EOF | sudo tee /etc/cron.d/pg_dump
+
+SHELL=/usr/bin/bash
+# HP Data protector corre a las 10am, 1pm, 3pm y 10pm.
+# Nosotros, 10 minutos antes. pg_dump solo dura ~5s.
+
+50 9,12,15,20 * * * postgres /usr/bin/pg_dump -Fc -U postgres alfresco -f /mnt/e/alfresco_pgsql/respaldo-bd-\$(date +\%F-\%s).sql.gz 2>&1 > /dev/null
 
 
+# Limpiamos respaldos todos los días
 30 9 * * * postgres /usr/bin/rm /mnt/e/alfresco_pgsql/*.sql.gz
-
 EOF
-
