3aeac18615d665519e961b0a4be648a1859e5a24
Alvaro Figueroa <alvaro@greencore.co.cr>
2021-03-16 17:39:24 -0600

    Fixing backup dir in cron

--- scripts/start-backup.sh
+++ scripts/start-backup.sh
@@ -13,13 +13,13 @@ SHELL=/usr/bin/bash
 # Se deben copiar de forma manual hacia el equipo destino
 
 # Realizamos respaldo de alfresco y trazas 
-50 20 * * * postgres /usr/bin/pg_dump -Fc -U postgres alfresco -f /mnt/e/alfresco_pgsql/respaldo-bd-\$(date +\%F-\%s).sql.gz 2>&1 > /dev/null
-50 20 * * * postgres /usr/bin/pg_dump -Fc -U postgres trazas -f /mnt/e/alfresco_pgsql/respaldo-trazas-\$(date +\%F-\%s).sql.gz 2>&1 > /dev/null
+50 20 * * * postgres /usr/bin/pg_dump -Fc -U postgres alfresco -f /mnt/e/alfresco-backup/respaldo-bd-\$(date +\%F-\%s).sql.gz 2>&1 > /dev/null
+50 20 * * * postgres /usr/bin/pg_dump -Fc -U postgres trazas -f /mnt/e/alfresco-backup/respaldo-trazas-\$(date +\%F-\%s).sql.gz 2>&1 > /dev/null
 
 # Realizamos tar para no perder permisos por WSL
-0 20 * * * root tar cJf /mnt/e/alfresco-backup-\$(date +\%F-\%s).tar.xz /opt/alfresco/
+0 20 * * * root tar cJf /mnt/e/alfresco-backup/alfresco-backup-\$(date +\%F-\%s).tar.xz /opt/alfresco/
 
 # Limpiamos respaldos todos los días
-30 19 * * * postgres /usr/bin/rm /mnt/e/alfresco_pgsql/*.sql.gz
+30 19 * * * postgres /usr/bin/rm /mnt/e/alfresco-backup/*
 
 EOF
